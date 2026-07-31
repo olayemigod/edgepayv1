b32a026706e4983f027d855ed1ab25749d0f7989
olayemigod <olayemigod@gmail.com>
2026-07-31 20:13:29 +0100

    feat(edgepay): validate merchant account and branch context

--- edgepayv1/edgepay/doctype/edgepay_payment_request/edgepay_payment_request.py
+++ edgepayv1/edgepay/doctype/edgepay_payment_request/edgepay_payment_request.py
@@ -4,6 +4,8 @@ from frappe import _
 from frappe.model.document import Document
 from frappe.utils import flt
 
+from edgepayv1.edgepay.services.merchant_queries import validate_branch_context
+
 
 class EdgePayPaymentRequest(Document):
 	def validate(self):
@@ -17,12 +19,21 @@ class EdgePayPaymentRequest(Document):
 			frappe.throw(_("Merchant is required"))
 		if not self.provider_account:
 			frappe.throw(_("Provider Account is required"))
+		validate_branch_context(self.merchant, self.merchant_account, self.merchant_branch)
 		account = frappe.get_doc("EdgePay Provider Account", self.provider_account)
 		if account.merchant != self.merchant:
 			frappe.throw(_("Provider Account does not belong to the selected Merchant"))
 		if self.provider and account.provider != self.provider:
 			frappe.throw(_("Provider Account does not match the selected Provider"))
 		self.provider = account.provider
+		if self.merchant_account and not self.external_company_reference:
+			self.external_company_reference = frappe.db.get_value(
+				"EdgePay Merchant Account", self.merchant_account, "external_company_reference"
+			)
+		if self.merchant_branch and not self.external_branch_reference:
+			self.external_branch_reference = frappe.db.get_value(
+				"EdgePay Merchant Branch", self.merchant_branch, "external_branch_reference"
+			)
 
 	def validate_idempotency_key(self):
 		if self.idempotency_key:
