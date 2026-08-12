2ab65117c8c8516c4c11ddd59bf229b8498eebb2
olayemigod <olayemigod@gmail.com>
2026-08-12 21:11:59 +0100

    style(edgepay): format integrations shell loader

--- edgepayv1/edgepay/page/edgepay_integrations/edgepay_integrations.js
+++ edgepayv1/edgepay/page/edgepay_integrations/edgepay_integrations.js
@@ -1,13 +1,13 @@
-frappe.pages['edgepay-integrations'].on_page_load = function (wrapper) {
+frappe.pages["edgepay-integrations"].on_page_load = function (wrapper) {
 	wrapper.page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: __('Integrations'),
+		title: __("Integrations"),
 		single_column: true,
 	});
 };
 
-frappe.pages['edgepay-integrations'].on_page_show = function (wrapper) {
-	frappe.require('/assets/edgepayv1/js/edgepay_page_loader.js', () => {
-		window.EdgePayPageLoader.mount(wrapper, 'integrations');
+frappe.pages["edgepay-integrations"].on_page_show = function (wrapper) {
+	frappe.require("/assets/edgepayv1/js/edgepay_page_loader.js", () => {
+		window.EdgePayPageLoader.mount(wrapper, "integrations");
 	});
 };
