476f987c371fda987fb2b58b90203de2e7389689
olayemigod <olayemigod@gmail.com>
2026-08-12 21:10:50 +0100

    style(edgepay): format home shell loader

--- edgepayv1/edgepay/page/edgepay_home/edgepay_home.js
+++ edgepayv1/edgepay/page/edgepay_home/edgepay_home.js
@@ -1,13 +1,13 @@
-frappe.pages['edgepay-home'].on_page_load = function (wrapper) {
+frappe.pages["edgepay-home"].on_page_load = function (wrapper) {
 	wrapper.page = frappe.ui.make_app_page({
 		parent: wrapper,
-		title: __('EdgePay Home'),
+		title: __("EdgePay Home"),
 		single_column: true,
 	});
 };
 
-frappe.pages['edgepay-home'].on_page_show = function (wrapper) {
-	frappe.require('/assets/edgepayv1/js/edgepay_page_loader.js', () => {
-		window.EdgePayPageLoader.mount(wrapper, 'home');
+frappe.pages["edgepay-home"].on_page_show = function (wrapper) {
+	frappe.require("/assets/edgepayv1/js/edgepay_page_loader.js", () => {
+		window.EdgePayPageLoader.mount(wrapper, "home");
 	});
 };
